7d6d4d3dc0539d8698258985686253a1c556a185
dilshodev3234 <Adilshod3234@gmail.com>
2023-07-12 08:53:39 +0500

    Initial commit

---
+++ imtihon_bajarish_tartibi.py
@@ -0,0 +1,20 @@
+"""
+ism_familya.txt:
+    1) link
+    code ....
+
+
+    2) link
+    code ....
+
+
+    3) code ....
+
+    4) code ....
+
+    5) query code ....
+
+
+
+git hub account ga yozgan imtihon javoblarizni push qilasiz!
+"""
